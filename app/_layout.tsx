0c034ee7c10cdc1e09054f5ef93e4c30ccbd72f4
Dmitriy Guryanov <dmitriy.guryanov@icloud.com>
2025-08-18 18:29:22 +0500

    Implemented _layout.tsx

---
+++ app/_layout.tsx
@@ -0,0 +1,39 @@
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { MarkerProvider } from '../contexts/MarkerContext';
+
+export default function RootLayout() {
+    return (
+        <MarkerProvider>
+            <StatusBar style = "auto" />
+            <Stack>
+                <Stack.Screen
+                name = "index"
+                options = {{
+                    title: "Карта",
+                    headerStyle: {
+                        backgroundColor: "#fff",
+                    },
+                    headerTintColor: "#000",
+                    headerTitleStyle: {
+                        fontWeight: "medium",
+                    },
+                }}
+                />
+                <Stack.Screen
+                name = "marker/[id]"
+                options = {{
+                    title: "Детали маркера",
+                    headerStyle: {
+                        backgroundColor: "#fff",
+                    },
+                    headerTintColor: "#000",
+                    headerTitleStyle: {
+                        fontWeight: "medium",
+                    },
+                }}
+                />
+            </Stack>
+        </MarkerProvider>
+    );
+}
